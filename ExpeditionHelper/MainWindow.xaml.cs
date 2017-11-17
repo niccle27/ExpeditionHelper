abb12ce24434bf54ace3fa9906d87a96d553f19f
niccle27 <niccle27@gmail.com>
2017-11-17 13:01:15 +0100

    add utilisateur class

--- ExpeditionHelper/MainWindow.xaml.cs
+++ ExpeditionHelper/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace ExpeditionHelper
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             ManagerSql.HydrateCategorie();
---
+++ ExpeditionHelper/Utilisateur.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionHelper
+{
+    class Utilisateur
+    {
+        public Utilisateur()
+        {
+
+        }
+        public Utilisateur(int id_utilisateur,string login,string password)
+        {
+            this.id_Utilisateur = id_utilisateur;
+            this.login = login;
+            this.password = password;
+        }
+        public void hydrate(int id_utilisateur, string login, string password)
+        {
+            this.id_Utilisateur = id_utilisateur;
+            this.login = login;
+            this.password = password;
+        }
+        private int id_Utilisateur;
+        public int Id_utilisateur
+        {
+            get { return id_Utilisateur; }
+            set { id_Utilisateur = value; }
+        }
+        private string login;
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+
+
+    }
+}
